bfbba5a5f15f4568797bf67513014b94beddaa14
Justin Rainbow <justin.rainbow@sheknows.com>
2011-03-15 18:14:01 -0700

    Updates to bootstrap - removing test includes from main bootstrap file

--- bootstrap.php
+++ bootstrap.php
@@ -2,4 +2,4 @@
 
 require 'libs/JsonSchema.php';
 require 'libs/JsonSchemaUndefined.php';
-require 'tests/BaseTestCase.php';
+
---
+++ tests/bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+
+require dirname(__FILE__) . '/../bootstrap.php';
+require 'BaseTestCase.php';
