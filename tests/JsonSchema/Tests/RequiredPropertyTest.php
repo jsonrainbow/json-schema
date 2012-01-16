4f04966e5ba4015bf6e3a2ced05a3a1c0570e5bc
Morf <i@morfi.ru>
2012-01-16 16:32:23 +0400

    Extended "Required Property" test

--- tests/JsonSchema/Tests/RequiredPropertyTest.php
+++ tests/JsonSchema/Tests/RequiredPropertyTest.php
@@ -41,6 +41,50 @@ class RequiredPropertyTest extends BaseTestCase
                     "number":{"type":"string"}
                   }
                 }'
+            ),
+    		array(
+                '{
+				 "number": 0
+				}',
+                '{
+                  "type":"object",
+                  "properties":{
+                    "number":{"type":"integer","required":true}
+                  }
+                }'
+            ),
+			array(
+                '{
+				 "is_active": false
+				}',
+                '{
+                  "type":"object",
+                  "properties":{
+                    "is_active":{"type":"boolean","required":true}
+                  }
+                }'
+            ),
+			array(
+                '{
+				 "status": null
+				}',
+                '{
+                  "type":"object",
+                  "properties":{
+                    "status":{"type":"null","required":true}
+                  }
+                }'
+            ),
+			array(
+                '{
+				 "users": []
+				}',
+                '{
+                  "type":"object",
+                  "properties":{
+                    "users":{"type":"array","required":true}
+                  }
+                }'
             )
         );
     }
