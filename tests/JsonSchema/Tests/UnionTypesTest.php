2003956d7ffb58d0884878ef743517ee7b5ddb1f
Justin Rainbow <justin.rainbow@gmail.com>
2012-05-13 12:05:34 -0700

    Adding header license text for all files

--- tests/JsonSchema/Tests/UnionTypesTest.php
+++ tests/JsonSchema/Tests/UnionTypesTest.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file is part of the JsonSchema package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace JsonSchema\Tests;
 
 class UnionTypesTest extends BaseTestCase
