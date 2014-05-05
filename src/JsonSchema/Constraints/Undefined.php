jsonrainbow/json-schema

commit 61059b29dd372a1492a3af74b76a38c36df33644

refactor conditional logic of 'required' validation

src/JsonSchema/Constraints/Undefined.php
@@ -109,20 +109,20 @@ class Undefined extends Constraint
 
         // Verify required values
         if (is_object($value)) {
-            if ($value instanceof Undefined) {
-                // Draft 3 - Required attribute - e.g. "foo": {"type": "string", "required": true}
-                if (isset($schema->required) && $schema->required) {
-                    $this->addError($path, "is missing and it is required");
-                }
-            } else if (isset($schema->required)) {
-                if( is_array($schema->required)) {
-                    // Draft 4 - Required is an array of strings - e.g. "required": ["foo", ...]
-                    foreach ($schema->required as $required) {
-                        if (!property_exists($value, $required)) {
-                            $this->addError($path, "the property " . $required . " is required");
-                        }
+            if (isset($schema->required) && is_array($schema->required) ) {
+                // Draft 4 - Required is an array of strings - e.g. "required": ["foo", ...]
+                foreach ($schema->required as $required) {
+                    if (!property_exists($value, $required)) {
+                        $this->addError($path, "the property " . $required . " is required");
                     }
                 }
+            } else if (isset($schema->required)) {
+                // Draft 3 - Required attribute - e.g. "foo": {"type": "string", "required": true}
+                if ( $schema->required && $value instanceof Undefined) {
+                    $this->addError($path, "is missing and it is required");
+                }
+            } else if ($value instanceof Undefined) {
+                // don't check type of Undefined value
             } else {
                 $this->checkType($value, $schema, $path);
             }

commit e8a72f0018f28379bf832e7c891394be67fbc1f6

dont validate draft4 required attribute when value is undefined
- fixes regression introduced by 61059b2

src/JsonSchema/Constraints/Undefined.php
@@ -117,14 +117,15 @@ class Undefined extends Constraint
 
         // Verify required values
         if (is_object($value)) {
-            if (isset($schema->required) && is_array($schema->required) ) {
+
+            if (!($value instanceof Undefined) && isset($schema->required) && is_array($schema->required) ) {
                 // Draft 4 - Required is an array of strings - e.g. "required": ["foo", ...]
                 foreach ($schema->required as $required) {
                     if (!property_exists($value, $required)) {
                         $this->addError($path, "the property " . $required . " is required");
                     }
                 }
-            } else if (isset($schema->required)) {
+            } else if (isset($schema->required) && !is_array($schema->required)) {
                 // Draft 3 - Required attribute - e.g. "foo": {"type": "string", "required": true}
                 if ( $schema->required && $value instanceof Undefined) {
                     $this->addError($path, "is missing and it is required");