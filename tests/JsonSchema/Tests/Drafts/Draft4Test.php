fcae137ad476f9db4c6c923bc5d34dcc4fe91684
ivan <ivan@pmovil.com.br>
2014-10-20 03:23:12 -0200

    Test case for inner definitions

--- tests/JsonSchema/Tests/Drafts/Draft4Test.php
+++ tests/JsonSchema/Tests/Drafts/Draft4Test.php
@@ -25,5 +25,39 @@ class Draft4Test extends BaseDraftTestCase
             'zeroTerminatedFloats.json'
         );
     }
+    
+    public function testInnerDefinitions(){
+        $schema = <<<JSN
+{
+    "type": "object",
+    "additionalProperties":false,
+    "properties": {
+        "person": { "\$ref": "#/definitions/persondef" }
+    },
+    "definitions": {
+        "persondef": {
+            "type": "object",
+            "additionalProperties":false,
+            "properties": {
+                "name": {
+                    "type": "string"
+                },
+                "age": {
+                    "type" : "integer"
+                }
+            }
+        }
+    }
+}
+JSN;
+        $schemaObj = json_decode($schema);
+        $resolver = new \JsonSchema\RefResolver();
+        $resolver->resolve($schemaObj);
+        
+        $schema = json_encode($schemaObj);
+        
+        $this->testValidCases('{"person": {"name" : "John Doe", "age" : 30} }', $schema);
+        $this->testInvalidCases('{"person": {"name" : "John Doe", "age" : "wrong"} }', $schema);
+    }
 
 }
