14443c5bcaae4cf4f94798aa55bc7a110524eba8
loucho <loucho@redmantech.com>
2015-03-26 17:17:48 -0600

    Check the element type before making the comparison (== for objects, === for everything else)

--- src/JsonSchema/Constraints/EnumConstraint.php
+++ src/JsonSchema/Constraints/EnumConstraint.php
@@ -23,13 +23,21 @@ class EnumConstraint extends Constraint
     public function check($element, $schema = null, $path = null, $i = null)
     {
         // Only validate enum if the attribute exists
-        if ($element instanceof UndefinedConstraint && (!isset($schema->required) || !$schema->required))  {
+        if ($element instanceof UndefinedConstraint && (!isset($schema->required) || !$schema->required)) {
             return;
         }
 
         foreach ($schema->enum as $enum) {
-            if ((gettype($element) === gettype($enum)) && ($element === $enum)) {
-                return;
+            $type = gettype($element);
+            if ($type === gettype($enum)) {
+                if ($type == "object") {
+                    if ($element == $enum)
+                        return;
+                } else {
+                    if ($element === $enum)
+                        return;
+
+                }
             }
         }
 
