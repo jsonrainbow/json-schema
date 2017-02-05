f55a09b5e1ea574c3c481f1c34287a04c4b7c4e4
Erayd <steve@erayd.net>
2017-02-05 14:58:05 +1300

    Add option to throw an exception on validation errors

---
+++ tests/Constraints/ValidationExceptionTest.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the JsonSchema package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace JsonSchema\Tests\Constraints;
+
+use JsonSchema\Validator;
+use JsonSchema\Constraints\Constraint;
+use JsonSchema\Exception\ValidationException;
+
+class ValidationExceptionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testValidationException()
+    {
+        $exception = new ValidationException();
+        $this->assertInstanceOf('\JsonSchema\Exception\ValidationException', $exception);
+
+        $checkValue = json_decode('{"propertyOne": "thisIsNotAnObject"}');
+        $schema = json_decode('{
+            "type": "object",
+            "additionalProperties": false,
+            "properties": {
+                "propertyOne": {
+                    "type": "object"
+                }
+            }
+        }');
+
+        $validator = new Validator();
+
+        try {
+            $validator->validate($checkValue, $schema, Constraint::CHECK_MODE_EXCEPTIONS);
+        } catch (\Exception $e) {
+            $exception = $e;
+        }
+
+        $this->assertEquals(
+            'Error validating /propertyOne: String value found, but an object is required',
+            $exception->getMessage()
+        );
+
+
+        $this->setExpectedException('JsonSchema\Exception\ValidationException');
+        throw $exception;
+        
+    }
+}
