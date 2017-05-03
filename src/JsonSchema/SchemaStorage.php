048340601df50bf3966d0b544021c561dd04f50e
Erayd <steve@erayd.net>
2017-05-04 09:43:03 +1200

    Correct format for id & $ref in draft-03/04 meta-schemas
    See https://github.com/json-schema-org/JSON-Schema-Test-Suite/issues/177#issuecomment-293051367

--- src/JsonSchema/SchemaStorage.php
+++ src/JsonSchema/SchemaStorage.php
@@ -58,6 +58,17 @@ class SchemaStorage implements SchemaStorageInterface
             $schema = BaseConstraint::arrayToObjectRecursive($schema);
         }
 
+        // workaround for bug in draft-03 & draft-04 meta-schemas (id & $ref defined with incorrect format)
+        // see https://github.com/json-schema-org/JSON-Schema-Test-Suite/issues/177#issuecomment-293051367
+        if (is_object($schema) && property_exists($schema, 'id')) {
+            if ($schema->id == 'http://json-schema.org/draft-04/schema#') {
+                $schema->properties->id->format = 'uri-reference';
+            } elseif ($schema->id == 'http://json-schema.org/draft-03/schema#') {
+                $schema->properties->id->format = 'uri-reference';
+                $schema->properties->{'$ref'}->format = 'uri-reference';
+            }
+        }
+
         $objectIterator = new ObjectIterator($schema);
         foreach ($objectIterator as $toResolveSchema) {
             if (property_exists($toResolveSchema, '$ref') && is_string($toResolveSchema->{'$ref'})) {
