a139c54e6467691c1bbe3f6f9faf53566d6b79d9
Erayd <steve@erayd.net>
2017-06-27 19:50:33 +1200

    Comments for JsonSchema\Enum

--- src/JsonSchema/Enum.php
+++ src/JsonSchema/Enum.php
@@ -2,6 +2,17 @@
 
 namespace JsonSchema;
 
+/**
+ * Alias class for \MabeEnum\Enum, so that the underlying class can be easily changed or extended.
+ *
+ * This library currently contains no custom functionality, but defines this class so that
+ * the Enum interface can be preserved if the upstream API changes, or a different Enum package
+ * is used.
+ *
+ * @package justinrainbow\json-schema
+ *
+ * @license MIT
+ */
 abstract class Enum extends \MabeEnum\Enum
 {
 }
