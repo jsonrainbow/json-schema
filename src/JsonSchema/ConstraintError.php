6e0a1772654ea04e2f04246a5aae946ba63a25fa
Peter Mead <peterm@hq.azuragroup.com>
2017-11-28 16:48:30 +0000

    Don't cast multipleOf to be an integer for the error message
    While this isn't the best way to deal with multipleOf being either an integer or a float it should suffice for now.

--- src/JsonSchema/ConstraintError.php
+++ src/JsonSchema/ConstraintError.php
@@ -88,7 +88,7 @@ class ConstraintError extends Enum
             self::MISSING_MAXIMUM => 'Use of exclusiveMaximum requires presence of maximum',
             self::MISSING_MINIMUM => 'Use of exclusiveMinimum requires presence of minimum',
             /*self::MISSING_ERROR => 'Used for tests; this error is deliberately commented out',*/
-            self::MULTIPLE_OF => 'Must be a multiple of %d',
+            self::MULTIPLE_OF => 'Must be a multiple of %s',
             self::NOT => 'Matched a schema which it should not',
             self::ONE_OF => 'Failed to match exactly one schema',
             self::REQUIRED => 'The property %s is required',
