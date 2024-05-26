4f0e8f2e1d80c3411d2f7047007af9710fcf837e
Danny van der Sluijs <danny.vandersluijs@icloud.com>
2024-05-26 21:23:33 +0200

    style: Correct code style violations

--- tests/Uri/Retrievers/FileGetContentsTest.php
+++ tests/Uri/Retrievers/FileGetContentsTest.php
@@ -1,50 +1,49 @@
 <?php
 
-namespace JsonSchema\Tests\Uri\Retrievers
+namespace JsonSchema\Tests\Uri\Retrievers;
+
+use JsonSchema\Uri\Retrievers\FileGetContents;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group FileGetContents
+ */
+class FileGetContentsTest extends TestCase
 {
-    use JsonSchema\Uri\Retrievers\FileGetContents;
-    use PHPUnit\Framework\TestCase;
-
     /**
-     * @group FileGetContents
+     * @expectedException \JsonSchema\Exception\ResourceNotFoundException
      */
-    class FileGetContentsTest extends TestCase
+    public function testFetchMissingFile()
     {
-        /**
-         * @expectedException \JsonSchema\Exception\ResourceNotFoundException
-         */
-        public function testFetchMissingFile()
-        {
-            $res = new FileGetContents();
-            $res->retrieve(__DIR__ . '/Fixture/missing.json');
-        }
+        $res = new FileGetContents();
+        $res->retrieve(__DIR__ . '/Fixture/missing.json');
+    }
 
-        public function testFetchFile()
-        {
-            $res = new FileGetContents();
-            $result = $res->retrieve(__DIR__ . '/../Fixture/child.json');
-            $this->assertNotEmpty($result);
-        }
+    public function testFetchFile()
+    {
+        $res = new FileGetContents();
+        $result = $res->retrieve(__DIR__ . '/../Fixture/child.json');
+        $this->assertNotEmpty($result);
+    }
 
-        public function testContentType()
-        {
-            $res = new FileGetContents();
+    public function testContentType()
+    {
+        $res = new FileGetContents();
 
-            $reflector = new \ReflectionObject($res);
-            $fetchContentType = $reflector->getMethod('fetchContentType');
-            $fetchContentType->setAccessible(true);
+        $reflector = new \ReflectionObject($res);
+        $fetchContentType = $reflector->getMethod('fetchContentType');
+        $fetchContentType->setAccessible(true);
 
-            $this->assertTrue($fetchContentType->invoke($res, array('Content-Type: application/json')));
-            $this->assertFalse($fetchContentType->invoke($res, array('X-Some-Header: whateverValue')));
-        }
+        $this->assertTrue($fetchContentType->invoke($res, array('Content-Type: application/json')));
+        $this->assertFalse($fetchContentType->invoke($res, array('X-Some-Header: whateverValue')));
+    }
 
-        public function testCanHandleHttp301PermanentRedirect()
-        {
-            $res = new FileGetContents();
+    public function testCanHandleHttp301PermanentRedirect()
+    {
+        $res = new FileGetContents();
 
-            $res->retrieve('http://asyncapi.com/definitions/2.0.0/asyncapi.json');
+        $res->retrieve('http://asyncapi.com/definitions/2.0.0/asyncapi.json');
 
-            $this->assertSame('application/schema+json', $res->getContentType());
-        }
+        $this->assertSame('application/schema+json', $res->getContentType());
     }
 }
