61c2dd8fb078863308f08e925a25e926dfbd68f8
Danny van der Sluijs <danny.van.der.sluijs@infi.nl>
2024-10-08 11:27:29 +0200

    refactor: add declare strict_types to all classes in ./src

--- src/JsonSchema/Constraints/TypeCheck/TypeCheckInterface.php
+++ src/JsonSchema/Constraints/TypeCheck/TypeCheckInterface.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace JsonSchema\Constraints\TypeCheck;
 
 interface TypeCheckInterface
--- src/JsonSchema/Uri/Retrievers/PredefinedArray.php
+++ src/JsonSchema/Uri/Retrievers/PredefinedArray.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace JsonSchema\Uri\Retrievers;
 
 use JsonSchema\Validator;
