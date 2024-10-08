2b26e316fc265a2b7d84b29c9706ad6973811bc5
Danny van der Sluijs <danny.van.der.sluijs@infi.nl>
2024-10-08 11:27:29 +0200

    refactor: add declare strict_types to all classes in ./src

--- src/JsonSchema/Constraints/TypeCheck/StrictTypeCheck.php
+++ src/JsonSchema/Constraints/TypeCheck/StrictTypeCheck.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace JsonSchema\Constraints\TypeCheck;
 
 class StrictTypeCheck implements TypeCheckInterface
--- src/JsonSchema/Enum.php
+++ src/JsonSchema/Enum.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace JsonSchema;
 
 abstract class Enum extends \MabeEnum\Enum
