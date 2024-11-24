1702f6dd8fee88dc6b5c290ad6bfc37f5719cf5f
Danny van der Sluijs <danny.vandersluijs@icloud.com>
2024-11-24 23:11:28 +0100

    refactor: Cleanup test from @param annotations

--- tests/Rfc3339Test.php
+++ tests/Rfc3339Test.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace JsonSchema\Tests;
 
 use JsonSchema\Rfc3339;
@@ -8,11 +10,9 @@ use PHPUnit\Framework\TestCase;
 class Rfc3339Test extends TestCase
 {
     /**
-     * @param string         $string
-     * @param \DateTime|null $expected
      * @dataProvider provideValidFormats
      */
-    public function testCreateFromValidString($string, \DateTime $expected): void
+    public function testCreateFromValidString(string $string, \DateTime $expected): void
     {
         $actual = Rfc3339::createFromString($string);
 
@@ -21,56 +21,51 @@ class Rfc3339Test extends TestCase
     }
 
     /**
-     * @param string $string
      * @dataProvider provideInvalidFormats
      */
-    public function testCreateFromInvalidString($string): void
+    public function testCreateFromInvalidString(string $string): void
     {
         $this->assertNull(Rfc3339::createFromString($string), sprintf('String "%s" should not be converted to DateTime', $string));
     }
 
-    public function provideValidFormats(): array
+    public static function provideValidFormats(): \Generator
     {
-        return [
-            [
-                '2000-05-01T12:12:12Z',
-                \DateTime::createFromFormat('Y-m-d\TH:i:s', '2000-05-01T12:12:12', new \DateTimeZone('UTC'))
-            ],
-            [
-                '2000-05-01T12:12:12+0100',
-                \DateTime::createFromFormat('Y-m-d\TH:i:sP', '2000-05-01T12:12:12+01:00')
-            ],
-            [
-                '2000-05-01T12:12:12+01:00',
-                \DateTime::createFromFormat('Y-m-d\TH:i:sP', '2000-05-01T12:12:12+01:00')
-            ],
-            [
-                '2000-05-01T12:12:12.123456Z',
-                \DateTime::createFromFormat('Y-m-d\TH:i:s.u', '2000-05-01T12:12:12.123456', new \DateTimeZone('UTC'))
-            ],
-            [
-                '2000-05-01T12:12:12.123Z',
-                \DateTime::createFromFormat('Y-m-d\TH:i:s.u', '2000-05-01T12:12:12.123000', new \DateTimeZone('UTC'))
-            ],
-            [
-                '2000-05-01 12:12:12.123Z',
-                \DateTime::createFromFormat('Y-m-d H:i:s.u', '2000-05-01 12:12:12.123000', new \DateTimeZone('UTC'))
-            ],
-            [
-                '2000-05-01 12:12:12.123456Z',
-                \DateTime::createFromFormat('Y-m-d H:i:s.u', '2000-05-01 12:12:12.123456', new \DateTimeZone('UTC'))
-            ]
+        yield 'Zulu time' => [
+            '2000-05-01T12:12:12Z',
+            \DateTime::createFromFormat('Y-m-d\TH:i:s', '2000-05-01T12:12:12', new \DateTimeZone('UTC'))
+        ];
+        yield 'With time offset - without colon' => [
+            '2000-05-01T12:12:12+0100',
+            \DateTime::createFromFormat('Y-m-d\TH:i:sP', '2000-05-01T12:12:12+01:00')
+        ];
+        yield 'With time offset - with colon' => [
+            '2000-05-01T12:12:12+01:00',
+            \DateTime::createFromFormat('Y-m-d\TH:i:sP', '2000-05-01T12:12:12+01:00')
+        ];
+        yield 'Zulu time - with microseconds' => [
+            '2000-05-01T12:12:12.123456Z',
+            \DateTime::createFromFormat('Y-m-d\TH:i:s.u', '2000-05-01T12:12:12.123456', new \DateTimeZone('UTC'))
+        ];
+        yield 'Zulu time - with milliseconds' => [
+            '2000-05-01T12:12:12.123Z',
+            \DateTime::createFromFormat('Y-m-d\TH:i:s.u', '2000-05-01T12:12:12.123000', new \DateTimeZone('UTC'))
+        ];
+        yield 'Zulu time - with milliseconds, without T separator' => [
+            '2000-05-01 12:12:12.123Z',
+            \DateTime::createFromFormat('Y-m-d H:i:s.u', '2000-05-01 12:12:12.123000', new \DateTimeZone('UTC'))
+        ];
+        yield 'Zulu time - with microseconds, without T separator' => [
+            '2000-05-01 12:12:12.123456Z',
+            \DateTime::createFromFormat('Y-m-d H:i:s.u', '2000-05-01 12:12:12.123456', new \DateTimeZone('UTC'))
         ];
     }
 
-    public function provideInvalidFormats(): array
+    public static function provideInvalidFormats(): \Generator
     {
-        return [
-            ['1999-1-11T00:00:00Z'],
-            ['1999-01-11T00:00:00+100'],
-            ['1999-01-11T00:00:00+1:00'],
-            ['1999-01-01  00:00:00Z'],
-            ['1999-1-11 00:00:00Z']
-        ];
+        yield 'Missing leading zero in month - with T separator' => ['1999-1-11T00:00:00Z'];
+        yield 'Missing leading zero in timezone offset - without colon' => ['1999-01-11T00:00:00+100'];
+        yield 'Missing leading zero in timezone offset - with colon' => ['1999-01-11T00:00:00+1:00'];
+        yield 'Double space between date and time' => ['1999-01-01  00:00:00Z'];
+        yield 'Missing leading zero in month - without T separator' => ['1999-1-11 00:00:00Z'];
     }
 }
