5a63b4db3083c8216fbf3e3397cdfc130f90ba9d
Danny van der Sluijs <danny.van.der.sluijs@infi.nl>
2025-03-07 15:15:08 +0100

    refactor: replace icecase/parity with custom deep comparator
    https://github.com/jsonrainbow/json-schema/issues/753

---
+++ src/JsonSchema/Tool/DeepComparer.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JsonSchema\Tool;
+
+use phpDocumentor\Reflection\Types\True_;
+
+class DeepComparer
+{
+    public static function isEqual($left, $right): bool
+    {
+        $isLeftScalar = is_scalar($left);
+        $isRightScalar = is_scalar($right);
+
+        if ($isLeftScalar && $isRightScalar) {
+            return $left === $right;
+        }
+
+        if ($isLeftScalar !== $isRightScalar) {
+            return false;
+        }
+
+        if (is_array($left) && is_array($right)) {
+            return self::isArrayEqual($left, $right);
+        }
+
+        if ($left instanceof \stdClass && $right instanceof \stdClass) {
+            return self::isArrayEqual((array) $left, (array) $right);
+        }
+
+        return false;
+    }
+
+    private static function isArrayEqual(array $left, array $right): bool
+    {
+        if (count($left) !== count($right)) {
+            return false;
+        }
+        foreach ($left as $key => $value) {
+            if (!array_key_exists($key, $right)) {
+                return false;
+            }
+
+            if (!self::isEqual($value, $right[$key])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
