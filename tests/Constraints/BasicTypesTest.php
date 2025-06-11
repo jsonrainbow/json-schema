8276d51ca3fc9fea4a2adbbae17e572e304cf726
Danny van der Sluijs <danny.van.der.sluijs@infi.nl>
2025-06-11 20:11:16 +0200

    refactor: apply generator to basic types test

--- tests/Constraints/BasicTypesTest.php
+++ tests/Constraints/BasicTypesTest.php
@@ -14,137 +14,134 @@ class BasicTypesTest extends BaseTestCase
     protected $schemaSpec = 'http://json-schema.org/draft-03/schema#';
     protected $validateSchema = true;
 
-    public function getInvalidTests(): array
+    public function getInvalidTests(): \Generator
     {
-        return [
-            [
-                '{
-                  "string":null
-                }',
-                '{
-                  "type":"object",
-                  "properties": {
-                    "string":{"type":"string"}
-                  },
-                  "additionalProperties":false
-                }'
-            ],
-            [
-                '{
-                  "number":null
-                }',
-                '{
-                  "type":"object",
-                  "properties": {
-                    "number":{"type":"number"}
-                  },
-                  "additionalProperties":false
-                }'
-            ],
-            [
-                '{
-                  "integer":null
-                }',
-                '{
-                  "type":"object",
-                  "properties": {
-                    "integer":{"type":"integer"}
-                  },
-                  "additionalProperties":false
-                }'
-            ],
-            [
-                '{
-                  "boolean":null
-                }',
-                '{
-                  "type":"object",
-                  "properties": {
-                    "boolean":{"type":"boolean"}
-                  },
-                  "additionalProperties":false
-                }'
-            ],
-            [
-                '{
-                  "object":null
-                }',
-                '{
-                  "type":"object",
-                  "properties": {
-                    "object":{"type":"object"}
-                  },
-                  "additionalProperties":false
-                }'
-            ],
-            [
-                '{
-                  "array":null
-                }',
-                '{
-                  "type":"object",
-                  "properties": {
-                    "array":{"type":"array"}
-                  },
-                  "additionalProperties":false
-                }'
-            ],
-            [
-                '{
-                  "null":1
-                }',
-                '{
-                  "type":"object",
-                  "properties": {
-                    "null":{"type":"null"}
-                  },
-                  "additionalProperties":false
-                }'
-            ]
+
+        yield [
+            '{
+              "string":null
+            }',
+            '{
+              "type":"object",
+              "properties": {
+                "string":{"type":"string"}
+              },
+              "additionalProperties":false
+            }'
         ];
+        yield [
+            '{
+              "number":null
+            }',
+            '{
+              "type":"object",
+              "properties": {
+                "number":{"type":"number"}
+              },
+              "additionalProperties":false
+            }'
+        ];
+        yield [
+            '{
+              "integer":null
+            }',
+            '{
+              "type":"object",
+              "properties": {
+                "integer":{"type":"integer"}
+              },
+              "additionalProperties":false
+            }'
+        ];
+        yield [
+            '{
+              "boolean":null
+            }',
+            '{
+              "type":"object",
+              "properties": {
+                "boolean":{"type":"boolean"}
+              },
+              "additionalProperties":false
+            }'
+        ];
+        yield [
+            '{
+              "object":null
+            }',
+            '{
+              "type":"object",
+              "properties": {
+                "object":{"type":"object"}
+              },
+              "additionalProperties":false
+            }'
+        ];
+        yield [
+            '{
+              "array":null
+            }',
+            '{
+              "type":"object",
+              "properties": {
+                "array":{"type":"array"}
+              },
+              "additionalProperties":false
+            }'
+        ];
+        yield [
+            '{
+              "null":1
+            }',
+            '{
+              "type":"object",
+              "properties": {
+                "null":{"type":"null"}
+              },
+              "additionalProperties":false
+            }'
+            ];
     }
 
-    public function getValidTests(): array
+    public function getValidTests(): \Generator
     {
-        return [
-            [
-                '{
-                  "string":"string test",
-                  "number":1,
-                  "integer":1,
-                  "boolean":true,
-                  "object":{},
-                  "array":[],
-                  "null":null,
-                  "any": "string",
-                  "any1": 2.6,
-                  "any2": 4,
-                  "any3": false,
-                  "any4": {},
-                  "any5": [],
-                  "any6": null
-                }',
-                '{
-                  "type":"object",
-                  "properties":{
-                    "string":{"type":"string"},
-                    "number":{"type":"number"},
-                    "integer":{"type":"integer"},
-                    "boolean":{"type":"boolean"},
-                    "object":{"type":"object"},
-                    "array":{"type":"array"},
-                    "null":{"type":"null"},
-                    "any": {"type":"any"},
-                    "any1": {"type":"any"},
-                    "any2": {"type":"any"},
-                    "any3": {"type":"any"},
-                    "any4": {"type":"any"},
-                    "any5": {"type":"any"},
-                    "any6": {"type":"any"}
-                  },
-                  "additionalProperties":false
-                }'
-            ]
+        yield [
+            '{
+              "string":"string test",
+              "number":1,
+              "integer":1,
+              "boolean":true,
+              "object":{},
+              "array":[],
+              "null":null,
+              "any": "string",
+              "any1": 2.6,
+              "any2": 4,
+              "any3": false,
+              "any4": {},
+              "any5": [],
+              "any6": null
+            }',
+            '{
+              "type":"object",
+              "properties":{
+                "string":{"type":"string"},
+                "number":{"type":"number"},
+                "integer":{"type":"integer"},
+                "boolean":{"type":"boolean"},
+                "object":{"type":"object"},
+                "array":{"type":"array"},
+                "null":{"type":"null"},
+                "any": {"type":"any"},
+                "any1": {"type":"any"},
+                "any2": {"type":"any"},
+                "any3": {"type":"any"},
+                "any4": {"type":"any"},
+                "any5": {"type":"any"},
+                "any6": {"type":"any"}
+              },
+              "additionalProperties":false
+            }'
         ];
     }
 }
