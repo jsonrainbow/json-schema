993c5bf16b7bed730d47d7a2b298a0ff7d4845da
Danny van der Sluijs <danny.van.der.sluijs@infi.nl>
2025-06-11 20:24:59 +0200

    refactor: apply generator to divisible by test

--- tests/Constraints/DivisibleByTest.php
+++ tests/Constraints/DivisibleByTest.php
@@ -13,85 +13,81 @@ class DivisibleByTest extends BaseTestCase
 {
     protected $validateSchema = true;
 
-    public function getInvalidTests(): array
+    public function getInvalidTests(): \Generator
     {
-        return [
-            [
-                '{"value": 5.6333}',
-                '{
-                  "type":"object",
-                  "properties":{
-                    "value":{"type":"number","divisibleBy":3}
-                  }
-                }'
-            ],
-            [
-                '{"value": 35}',
-                '{
-                    "type": "object",
-                    "properties": {
-                        "value": {"type": "integer", "divisibleBy": 1.5}
-                    }
-                }'
-            ],
-            [
-                '{"value": 0.00751}',
-                '{
-                    "type": "object",
-                    "properties": {
-                        "value": {"type": "number", "divisibleBy": 0.0001}
-                    }
-                }'
-            ],
-            [
-                '{"value": 7}',
-                '{
-                    "type": "object",
-                    "properties": {
-                        "value": {"type": "integer", "divisibleBy": 2}
-                    }
-                }'
-            ]
+        yield [
+            '{"value": 5.6333}',
+            '{
+              "type":"object",
+              "properties":{
+                "value":{"type":"number","divisibleBy":3}
+              }
+            }'
+        ];
+        yield [
+            '{"value": 35}',
+            '{
+                "type": "object",
+                "properties": {
+                    "value": {"type": "integer", "divisibleBy": 1.5}
+                }
+            }'
+        ];
+        yield [
+            '{"value": 0.00751}',
+            '{
+                "type": "object",
+                "properties": {
+                    "value": {"type": "number", "divisibleBy": 0.0001}
+                }
+            }'
+        ];
+        yield [
+            '{"value": 7}',
+            '{
+                "type": "object",
+                "properties": {
+                    "value": {"type": "integer", "divisibleBy": 2}
+                }
+            }'
         ];
     }
 
-    public function getValidTests(): array
+    public function getValidTests(): \Generator
     {
-        return [
-            [
-                '{"value": 6}',
-                '{
-                  "type":"object",
-                  "properties":{
-                    "value":{"type":"number","divisibleBy":3}
-                  }
-                }'
-            ],
-            [
-                '{"value": 4.5}',
-                '{
-                    "type": "object",
-                    "properties": {
-                        "value": {"type": "number", "divisibleBy": 1.5}
-                    }
-                }'
-            ],
-            [
-                '{"value": 0.0075}',
-                '{
-                    "properties": {
-                        "value": {"type": "number", "divisibleBy": 0.0001}
-                    }
-                }'
-            ],
-            [
-                '{"value": 1}',
-                '{
-                    "properties": {
-                        "value": {"type": "number", "divisibleBy": 0.02}
-                    }
-                }'
-            ]
+        yield [
+            '{"value": 6}',
+            '{
+              "type":"object",
+              "properties":{
+                "value":{"type":"number","divisibleBy":3}
+              }
+            }'
+        ];
+        yield [
+            '{"value": 4.5}',
+            '{
+                "type": "object",
+                "properties": {
+                    "value": {"type": "number", "divisibleBy": 1.5}
+                }
+            }'
+        ];
+        yield [
+            '{"value": 0.0075}',
+            '{
+                "properties": {
+                    "value": {"type": "number", "divisibleBy": 0.0001}
+                }
+            }'
+        ];
+        yield [
+            '{"value": 1}',
+            '{
+                "properties": {
+                    "value": {"type": "number", "divisibleBy": 0.02}
+                }
+            }'
         ];
     }
 }
