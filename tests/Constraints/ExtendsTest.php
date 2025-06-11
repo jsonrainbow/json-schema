b2c3eb750db168f71403e355996565144c10fead
Danny van der Sluijs <danny.van.der.sluijs@infi.nl>
2025-06-11 20:30:44 +0200

    docs: remove file level reference to license

--- tests/Constraints/ExtendsTest.php
+++ tests/Constraints/ExtendsTest.php
@@ -1,12 +1,5 @@
 <?php
 
-/*
- * This file is part of the JsonSchema package.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace JsonSchema\Tests\Constraints;
 
 class ExtendsTest extends BaseTestCase
