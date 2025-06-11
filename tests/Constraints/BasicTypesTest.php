988ece110b509956af3ac7ca2a296b7a82cad65b
Danny van der Sluijs <danny.van.der.sluijs@infi.nl>
2025-06-11 20:11:51 +0200

    refactor: add missing type hints

--- tests/Constraints/BasicTypesTest.php
+++ tests/Constraints/BasicTypesTest.php
@@ -4,7 +4,9 @@ namespace JsonSchema\Tests\Constraints;
 
 class BasicTypesTest extends BaseTestCase
 {
+    /** @var string  */
     protected $schemaSpec = 'http://json-schema.org/draft-03/schema#';
+    /** @var bool  */
     protected $validateSchema = true;
 
     public function getInvalidTests(): \Generator
