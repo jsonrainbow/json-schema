3128028b68fab55793907806444e375c1575c57c
Danny van der Sluijs <danny.van.der.sluijs@infi.nl>
2025-06-11 20:21:46 +0200

    docs: remove file level license reference; move class comment to correct level

--- tests/Constraints/DisallowTest.php
+++ tests/Constraints/DisallowTest.php
@@ -1,21 +1,16 @@
 <?php
 
-/*
- * This file is part of the JsonSchema package.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace JsonSchema\Tests\Constraints;
 
+/**
+ * Schemas in these tests look like draft-03, but the 'disallow' patterns provided are in
+ * violation of the spec - 'disallow' as defined in draft-03 accepts the same values as the
+ * 'type' option, and cannot take arbitrary patterns. The implementation in this library is
+ * probably deliberate, but noting that it's invalid, schema validation has been disabled
+ * for these tests. The 'disallow' option was removed permanently in draft-04.
+ */
 class DisallowTest extends BaseTestCase
 {
-    // schemas in these tests look like draft-03, but the 'disallow' patterns provided are in
-    // violation of the spec - 'disallow' as defined in draft-03 accepts the same values as the
-    // 'type' option, and cannot take arbitrary patterns. The implementation in this library is
-    // probably deliberate, but noting that it's invalid, schema validation has been disabled
-    // for these tests. The 'disallow' option was removed permanently in draft-04.
     protected $schemaSpec = 'http://json-schema.org/draft-03/schema#';
     protected $validateSchema = false;
 
