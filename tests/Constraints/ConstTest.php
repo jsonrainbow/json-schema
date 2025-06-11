78c00eddac1a1953d34ba107edfad666a51b1b7d
Danny van der Sluijs <danny.van.der.sluijs@infi.nl>
2025-06-11 20:14:35 +0200

    refactor: add missing type hints

--- tests/Constraints/ConstTest.php
+++ tests/Constraints/ConstTest.php
@@ -5,7 +5,9 @@ namespace JsonSchema\Tests\Constraints;
 
 class ConstTest extends BaseTestCase
 {
+    /** @var string  */
     protected $schemaSpec = 'http://json-schema.org/draft-06/schema#';
+    /** @var bool  */
     protected $validateSchema = true;
 
     public function getInvalidTests(): \Generator
