732709817f6f6599e9a22664c92711ca4555aec5
Danny van der Sluijs <danny.van.der.sluijs@infi.nl>
2025-06-13 14:32:03 +0200

    refactor: apply generator in min max properties test

--- tests/Constraints/MinMaxPropertiesTest.php
+++ tests/Constraints/MinMaxPropertiesTest.php
@@ -15,137 +15,127 @@ class MinMaxPropertiesTest extends BaseTestCase
 {
     protected $validateSchema = true;
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getValidTests(): array
+    public function getValidTests(): \Generator
     {
-        return [
-            'Empty object with minProperties: 0' => [
-                'input' => '{
-                  "value": {}
-                }',
-                'schema' => '{
-                  "type": "object",
-                  "properties": {
-                    "value": {"type": "object", "minProperties": 0}
-                  }
-                }'
-            ],
-            'Empty object with maxProperties: 1' => [
-                'input' => '{
-                  "value": {}
-                }',
-                'schema' => '{
-                  "type": "object",
-                  "properties": {
-                    "value": {"type": "object", "maxProperties": 1}
-                  }
-                }'
-            ],
-            'Empty object with minProperties: 0 and maxProperties: 1' => [
-                'input' => '{
-                  "value": {}
-                }',
-                'schema' => '{
-                  "type": "object",
-                  "properties": {
-                    "value": {"type": "object", "minProperties": 0,"maxProperties": 1}
-                  }
-                }'
-            ],
-            'Object with two properties with minProperties: 1 and maxProperties: 2' => [
-                'input' => '{
-                  "value": {"foo": 1, "bar": 2}
-                }',
-                'schema' => '{
-                  "type": "object",
-                  "properties": {
-                    "value": {"type": "object", "minProperties": 1,"maxProperties": 2}
-                  }
-                }'
-            ],
-            'Empty array with minProperties: 1 and maxProperties: 2' => [
-                'input' => '{
-                  "value": []
-                }',
-                'schema' => '{
-                  "properties": {
-                    "value": {"minProperties": 1,"maxProperties": 2}
-                  }
-                }',
-                'checkMode' => Constraint::CHECK_MODE_NORMAL,
-            ],
-            'Array with two items with maxProperties: 1' => [
-                'input' => '{
-                  "value": [1, 2]
-                }',
-                'schema' => '{
-                  "properties": {
-                    "value": {"maxProperties": 1}
-                  }
-                }'
-            ],
+        yield 'Empty object with minProperties: 0' => [
+            'input' => '{
+              "value": {}
+            }',
+            'schema' => '{
+              "type": "object",
+              "properties": {
+                "value": {"type": "object", "minProperties": 0}
+              }
+            }'
+        ];
+        yield 'Empty object with maxProperties: 1' => [
+            'input' => '{
+              "value": {}
+            }',
+            'schema' => '{
+              "type": "object",
+              "properties": {
+                "value": {"type": "object", "maxProperties": 1}
+              }
+            }'
+        ];
+        yield 'Empty object with minProperties: 0 and maxProperties: 1' => [
+            'input' => '{
+              "value": {}
+            }',
+            'schema' => '{
+              "type": "object",
+              "properties": {
+                "value": {"type": "object", "minProperties": 0,"maxProperties": 1}
+              }
+            }'
+        ];
+        yield 'Object with two properties with minProperties: 1 and maxProperties: 2' => [
+            'input' => '{
+              "value": {"foo": 1, "bar": 2}
+            }',
+            'schema' => '{
+              "type": "object",
+              "properties": {
+                "value": {"type": "object", "minProperties": 1,"maxProperties": 2}
+              }
+            }'
+        ];
+        yield 'Empty array with minProperties: 1 and maxProperties: 2' => [
+            'input' => '{
+              "value": []
+            }',
+            'schema' => '{
+              "properties": {
+                "value": {"minProperties": 1,"maxProperties": 2}
+              }
+            }',
+            'checkMode' => Constraint::CHECK_MODE_NORMAL,
+        ];
+        yield 'Array with two items with maxProperties: 1' => [
+            'input' => '{
+              "value": [1, 2]
+            }',
+            'schema' => '{
+              "properties": {
+                "value": {"maxProperties": 1}
+              }
+            }'
         ];
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getInvalidTests(): array
+    public function getInvalidTests(): \Generator
     {
-        return [
-            'Empty object with minProperties: 1' => [
-                'input' => '{
-                  "value": {}
-                }',
-                'schema' => '{
-                  "type": "object",
-                  "properties": {
-                    "value": {"type": "object", "minProperties": 1}
-                  }
-                }'
-            ],
-            'Empty object with minProperties' => [
-                'input' => '{}',
-                'schema' => '{
-                  "type": "object",
-                  "properties": {
-                    "propertyOne": {
-                      "type": "string"
-                    },
-                    "propertyTwo": {
-                      "type": "string"
-                    }
-                  },
-                  "minProperties": 1
-                }'
-            ],
-            'Object with two properties with maxProperties: 1' => [
-                'input' => '{
-                  "value": {
-                    "propertyOne": "valueOne",
-                    "propertyTwo": "valueTwo"
-                  }
-                }',
-                'schema' => '{
-                  "type": "object",
-                  "properties": {
-                    "value": {"type": "object", "maxProperties": 1}
-                  }
-                }'
-            ],
-            'Object with two properties with minProperties: 1 and maxProperties: 2' => [
-                'input' => '{
-                  "value": {"foo": 1, "bar": 2, "baz": 3}
-                }',
-                'schema' => '{
-                  "type": "object",
-                  "properties": {
-                    "value": {"type": "object", "minProperties": 1,"maxProperties": 2}
-                  }
-                }'
-            ],
+        yield 'Empty object with minProperties: 1' => [
+            'input' => '{
+              "value": {}
+            }',
+            'schema' => '{
+              "type": "object",
+              "properties": {
+                "value": {"type": "object", "minProperties": 1}
+              }
+            }'
+        ];
+        yield 'Empty object with minProperties' => [
+            'input' => '{}',
+            'schema' => '{
+              "type": "object",
+              "properties": {
+                "propertyOne": {
+                  "type": "string"
+                },
+                "propertyTwo": {
+                  "type": "string"
+                }
+              },
+              "minProperties": 1
+            }'
+        ];
+        yield 'Object with two properties with maxProperties: 1' => [
+            'input' => '{
+              "value": {
+                "propertyOne": "valueOne",
+                "propertyTwo": "valueTwo"
+              }
+            }',
+            'schema' => '{
+              "type": "object",
+              "properties": {
+                "value": {"type": "object", "maxProperties": 1}
+              }
+            }'
+        ];
+        yield 'Object with two properties with minProperties: 1 and maxProperties: 2' => [
+            'input' => '{
+              "value": {"foo": 1, "bar": 2, "baz": 3}
+            }',
+            'schema' => '{
+              "type": "object",
+              "properties": {
+                "value": {"type": "object", "minProperties": 1,"maxProperties": 2}
+              }
+            }'
         ];
     }
 }
