96f5d5284d1a64014e90f3ae5b4e65779dfcb305
Danny van der Sluijs <danny.van.der.sluijs@infi.nl>
2025-06-13 15:53:55 +0200

    refactor: apply generator to not test

--- tests/Constraints/NotTest.php
+++ tests/Constraints/NotTest.php
@@ -13,90 +13,86 @@ class NotTest extends BaseTestCase
 {
     protected $validateSchema = true;
 
-    public function getInvalidTests(): array
+    public function getInvalidTests(): \Generator
     {
-        return [
-            [
-                '{
-                    "x": [1, 2]
-                }',
-                '{
-                    "properties": {
-                        "x": {
-                            "not": {
-                                "type": "array",
-                                "items": {"type": "integer"},
-                                "minItems": 2
-                            }
+        yield [
+            '{
+                "x": [1, 2]
+            }',
+            '{
+                "properties": {
+                    "x": {
+                        "not": {
+                            "type": "array",
+                            "items": {"type": "integer"},
+                            "minItems": 2
                         }
                     }
-                }'
-            ],
-            [ // check that a missing, required property is correctly validated
-                '{"y": "foo"}',
-                '{
-                    "type": "object",
-                    "required": ["x"],
-                    "properties": {
-                        "x": {
-                            "not": {
-                                "type": "null"
-                            }
+                }
+            }'
+        ];
+        yield 'check that a missing, required property is correctly validated' => [
+            '{"y": "foo"}',
+            '{
+                "type": "object",
+                "required": ["x"],
+                "properties": {
+                    "x": {
+                        "not": {
+                            "type": "null"
                         }
                     }
-                }'
-            ]
+                }
+            }'
         ];
     }
 
-    public function getValidTests(): array
+    public function getValidTests(): \Generator
     {
-        return [
-            [
-                '{
-                    "x": [1]
-                }',
-                '{
-                    "properties": {
-                        "x": {
-                            "not": {
-                                "type": "array",
-                                "items": {"type": "integer"},
-                                "minItems": 2
-                            }
+        yield [
+            '{
+                "x": [1]
+            }',
+            '{
+                "properties": {
+                    "x": {
+                        "not": {
+                            "type": "array",
+                            "items": {"type": "integer"},
+                            "minItems": 2
                         }
                     }
-                }'
-            ],
-            [
-                '{
-                    "x": ["foo", 2]
-                }',
-                '{
-                    "properties": {
-                        "x": {
-                            "not": {
-                                "type": "array",
-                                "items": {"type": "integer"},
-                                "minItems": 2
-                            }
+                }
+            }'
+        ];
+        yield [
+            '{
+                "x": ["foo", 2]
+            }',
+            '{
+                "properties": {
+                    "x": {
+                        "not": {
+                            "type": "array",
+                            "items": {"type": "integer"},
+                            "minItems": 2
                         }
                     }
-                }'
-            ],
-            [ // check that a missing, non-required property isn't validated
-                '{"y": "foo"}',
-                '{
-                    "type": "object",
-                    "properties": {
-                        "x": {
-                            "not": {
-                                "type": "null"
-                            }
+                }
+            }'
+        ];
+        yield "check that a missing, non-required property isn't validated" => [
+            '{"y": "foo"}',
+            '{
+                "type": "object",
+                "properties": {
+                    "x": {
+                        "not": {
+                            "type": "null"
                         }
                     }
-                }'
-            ]
+                }
+            }'
         ];
     }
 }
