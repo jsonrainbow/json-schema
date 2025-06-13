1ed5d555cb037ce9b3e6fbffe7ae3dd5094aaa8e
Danny van der Sluijs <danny.van.der.sluijs@infi.nl>
2025-06-13 14:35:19 +0200

    refactor: apply generator to minimum maximum tests

--- tests/Constraints/MinimumMaximumTest.php
+++ tests/Constraints/MinimumMaximumTest.php
@@ -13,164 +13,160 @@ class MinimumMaximumTest extends BaseTestCase
 {
     protected $validateSchema = true;
 
-    public function getInvalidTests(): array
+    public function getInvalidTests(): \Generator
     {
-        return [
-            [
-                '{
-                  "value":2
-                }',
-                '{
-                  "type":"object",
-                  "properties":{
-                    "value":{"type":"integer","minimum":4}
-                  }
-                }'
-            ],
-            [
-                '{"value": 3}',
-                '{
-                    "type": "object",
-                    "properties": {
-                        "value": {"type": "integer", "minimum": 3, "exclusiveMinimum": true}
-                    }
-                }'
-            ],
-            [
-                '{
-                  "value":16
-                }',
-                '{
-                  "type":"object",
-                  "properties":{
-                    "value":{"type":"integer","maximum":8}
-                  }
-                }'
-            ],
-            [
-                '{"value": 8}',
-                '{
-                    "type": "object",
-                    "properties": {
-                        "value": {"type": "integer", "maximum": 8, "exclusiveMaximum": true}
-                    }
-                }'
-            ],
-            [
-                '{"value": 4}',
-                '{
-                    "type": "object",
-                    "properties": {
-                        "value": {"type": "integer", "exclusiveMinimum": true}
-                    }
-                }'
-            ],
-            [
-                '{"value": 4}',
-                '{
-                    "type": "object",
-                    "properties": {
-                        "value": {"type": "integer", "exclusiveMaximum": true}
-                    }
-                }'
-            ],
-            [
-                '{"value": 4}',
-                '{
-                    "type": "object",
-                    "properties": {
-                        "value": {"type": "integer", "minimum": 5, "exclusiveMinimum": false}
-                    }
-                }'
-            ],
-            [
-                '{"value": 4}',
-                '{
-                    "properties": {
-                        "value": {"type": "integer", "maximum": 3, "exclusiveMaximum": false}
-                    }
-                }'
-            ],
-            [
-                '{"value": 0.00}',
-                '{
-                    "properties": {
-                        "value": {"type": "number", "minimum": 0, "exclusiveMinimum": true}
-                    }
-                }'
-            ],
-            [
-                '{"value": 0.00}',
-                '{
-                    "properties": {
-                        "value": {"type": "number", "maximum": 0, "exclusiveMaximum": true}
-                    }
-                }'
-            ]
+        yield [
+            '{
+              "value":2
+            }',
+            '{
+              "type":"object",
+              "properties":{
+                "value":{"type":"integer","minimum":4}
+              }
+            }'
+        ];
+        yield [
+            '{"value": 3}',
+            '{
+                "type": "object",
+                "properties": {
+                    "value": {"type": "integer", "minimum": 3, "exclusiveMinimum": true}
+                }
+            }'
+        ];
+        yield [
+            '{
+              "value":16
+            }',
+            '{
+              "type":"object",
+              "properties":{
+                "value":{"type":"integer","maximum":8}
+              }
+            }'
+        ];
+        yield [
+            '{"value": 8}',
+            '{
+                "type": "object",
+                "properties": {
+                    "value": {"type": "integer", "maximum": 8, "exclusiveMaximum": true}
+                }
+            }'
+        ];
+        yield [
+            '{"value": 4}',
+            '{
+                "type": "object",
+                "properties": {
+                    "value": {"type": "integer", "exclusiveMinimum": true}
+                }
+            }'
+        ];
+        yield [
+            '{"value": 4}',
+            '{
+                "type": "object",
+                "properties": {
+                    "value": {"type": "integer", "exclusiveMaximum": true}
+                }
+            }'
+        ];
+        yield [
+            '{"value": 4}',
+            '{
+                "type": "object",
+                "properties": {
+                    "value": {"type": "integer", "minimum": 5, "exclusiveMinimum": false}
+                }
+            }'
+        ];
+        yield [
+            '{"value": 4}',
+            '{
+                "properties": {
+                    "value": {"type": "integer", "maximum": 3, "exclusiveMaximum": false}
+                }
+            }'
+        ];
+        yield [
+            '{"value": 0.00}',
+            '{
+                "properties": {
+                    "value": {"type": "number", "minimum": 0, "exclusiveMinimum": true}
+                }
+            }'
+        ];
+        yield [
+            '{"value": 0.00}',
+            '{
+                "properties": {
+                    "value": {"type": "number", "maximum": 0, "exclusiveMaximum": true}
+                }
+            }'
         ];
     }
 
-    public function getValidTests(): array
+    public function getValidTests(): \Generator
     {
-        return [
-            [
-                '{
-                  "value":6
-                }',
-                '{
-                  "type":"object",
-                  "properties":{
-                    "value":{"type":"integer","minimum":4}
-                  }
-                }'
-            ],
-            [
-                '{
-                  "value":6
-                }',
-                '{
-                  "type":"object",
-                  "properties":{
-                    "value":{"type":"integer","maximum":8}
-                  }
-                }'
-            ],
-            [
-                '{"value": 6}',
-                '{
-                    "type": "object",
-                    "properties": {
-                        "value": {"type": "integer", "minimum": 6, "exclusiveMinimum": false}
-                    }
-                }'
-            ],
-            [
-                '{"value": 6}',
-                '{
-                    "type": "object",
-                    "properties": {
-                        "value": {"type": "integer", "maximum": 6, "exclusiveMaximum": false}
-                    }
-                }'
-            ],
-            [
-                '{"value": 6}',
-                '{
-                    "type": "object",
-                    "properties": {
-                        "value": {"type": "integer", "minimum": 6}
-                    }
-                }'
-            ],
-            [
-                '{"value": 6}',
-                '{
-                    "type": "object",
-                    "properties": {
-                        "value": {"type": "integer", "maximum": 6}
-                    }
-                }'
-            ]
+        yield [
+            '{
+              "value":6
+            }',
+            '{
+              "type":"object",
+              "properties":{
+                "value":{"type":"integer","minimum":4}
+              }
+            }'
+        ];
+        yield [
+            '{
+              "value":6
+            }',
+            '{
+              "type":"object",
+              "properties":{
+                "value":{"type":"integer","maximum":8}
+              }
+            }'
+        ];
+        yield [
+            '{"value": 6}',
+            '{
+                "type": "object",
+                "properties": {
+                    "value": {"type": "integer", "minimum": 6, "exclusiveMinimum": false}
+                }
+            }'
+        ];
+        yield [
+            '{"value": 6}',
+            '{
+                "type": "object",
+                "properties": {
+                    "value": {"type": "integer", "maximum": 6, "exclusiveMaximum": false}
+                }
+            }'
+        ];
+        yield [
+            '{"value": 6}',
+            '{
+                "type": "object",
+                "properties": {
+                    "value": {"type": "integer", "minimum": 6}
+                }
+            }'
+        ];
+        yield [
+            '{"value": 6}',
+            '{
+                "type": "object",
+                "properties": {
+                    "value": {"type": "integer", "maximum": 6}
+                }
+            }'
         ];
     }
 }
