5b0991fdd6ed6b18b30fe03be5d79e10c4a4a161
Danny van der Sluijs <danny.van.der.sluijs@infi.nl>
2025-06-13 14:27:31 +0200

    refactor: add missing type hints

--- tests/Constraints/MinLengthMaxLengthTest.php
+++ tests/Constraints/MinLengthMaxLengthTest.php
@@ -4,6 +4,7 @@ namespace JsonSchema\Tests\Constraints;
 
 class MinLengthMaxLengthTest extends BaseTestCase
 {
+    /** @var bool */
     protected $validateSchema = true;
 
     public function getInvalidTests(): \Generator
