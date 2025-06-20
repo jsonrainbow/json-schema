51e52c61bfd2c7f6728e76a741482175a870a5d9
Danny van der Sluijs <danny.van.der.sluijs@infi.nl>
2025-06-20 14:36:34 +0200

    refactor: apply generator to require test

--- tests/Constraints/RequireTest.php
+++ tests/Constraints/RequireTest.php
@@ -13,40 +13,36 @@ class RequireTest extends BaseTestCase
 {
     protected $validateSchema = true;
 
-    public function getInvalidTests(): array
+    public function getInvalidTests(): \Generator
     {
-        return [
-            [
-                '{
-                  "state":"DF"
-                }',
-                '{
-                  "type":"object",
-                  "properties":{
-                    "state":{"type":"string","requires":"city"},
-                    "city":{"type":"string"}
-                  }
-                }'
-            ]
+        yield [
+            '{
+              "state":"DF"
+            }',
+            '{
+              "type":"object",
+              "properties":{
+                "state":{"type":"string","requires":"city"},
+                "city":{"type":"string"}
+              }
+            }'
         ];
     }
 
-    public function getValidTests(): array
+    public function getValidTests(): \Generator
     {
-        return [
-            [
-                '{
-                  "state":"DF",
-                  "city":"Brasília"
-                }',
-                '{
-                  "type":"object",
-                  "properties":{
-                    "state":{"type":"string","requires":"city"},
-                    "city":{"type":"string"}
-                  }
-                }'
-            ]
+        yield [
+            '{
+              "state":"DF",
+              "city":"Brasília"
+            }',
+            '{
+              "type":"object",
+              "properties":{
+                "state":{"type":"string","requires":"city"},
+                "city":{"type":"string"}
+              }
+            }'
         ];
     }
 }
