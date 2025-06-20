16b0d9ae6f109af13720abdb4d1a99d57a7fd152
Danny van der Sluijs <danny.van.der.sluijs@infi.nl>
2025-06-20 16:15:56 +0200

    refactor: add missing type hints

--- tests/Constraints/WrongMessagesFailingTestCaseTest.php
+++ tests/Constraints/WrongMessagesFailingTestCaseTest.php
@@ -4,6 +4,7 @@ namespace JsonSchema\Tests\Constraints;
 
 class WrongMessagesFailingTestCaseTest extends BaseTestCase
 {
+    /** @var bool */
     protected $validateSchema = true;
 
     public function getInvalidTests(): \Generator
