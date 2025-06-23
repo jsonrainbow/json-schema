fb362201f3a07ff7678364a85aaf0f0e22914ead
Danny van der Sluijs <danny.van.der.sluijs@infi.nl>
2025-06-23 20:52:00 +0200

    docs: remove remaining file headers with license reference

--- tests/ConstraintErrorTest.php
+++ tests/ConstraintErrorTest.php
@@ -2,12 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of the JsonSchema package.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 namespace JsonSchema\Tests;
 
 use JsonSchema\ConstraintError;
