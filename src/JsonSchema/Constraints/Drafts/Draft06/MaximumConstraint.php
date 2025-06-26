78df5251dd87e47b2039bc98c478bb64cc32826e
Danny van der Sluijs <danny.vandersluijs@icloud.com>
2025-06-26 21:03:39 +0200

    feat: more support on draft-06 schema

--- src/JsonSchema/Constraints/Drafts/Draft06/MaximumConstraint.php
+++ src/JsonSchema/Constraints/Drafts/Draft06/MaximumConstraint.php
@@ -10,7 +10,7 @@ use JsonSchema\Constraints\Factory;
 use JsonSchema\Entity\ErrorBagProxy;
 use JsonSchema\Entity\JsonPointer;
 
-class ExclusiveMaximumConstraint implements ConstraintInterface
+class MaximumConstraint implements ConstraintInterface
 {
     use ErrorBagProxy;
 
@@ -21,18 +21,14 @@ class ExclusiveMaximumConstraint implements ConstraintInterface
 
     public function check(&$value, $schema = null, ?JsonPointer $path = null, $i = null): void
     {
-        if (!property_exists($schema, 'exclusiveMaximum')) {
+        if (!property_exists($schema, 'maximum')) {
             return;
         }
 
-        if (!is_numeric($value)) {
+        if ($value <= $schema->maximum) {
             return;
         }
 
-        if ($value < $schema->exclusiveMaximum) {
-            return;
-        }
-
-        $this->addError(ConstraintError::EXCLUSIVE_MAXIMUM(), $path, ['exclusiveMaximum' => $schema->exclusiveMaximum, 'found' => $value]);
+        $this->addError(ConstraintError::MAXIMUM(), $path, ['maximum' => $schema->maximum, 'found' => $value]);
     }
 }
