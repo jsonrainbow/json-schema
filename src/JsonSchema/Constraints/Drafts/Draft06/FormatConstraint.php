8f6b2dc300496f1bd6372c27a1f3b7fe402266d1
Danny van der Sluijs <danny.vandersluijs@icloud.com>
2025-06-27 11:15:18 +0200

    feat: add more support for draft06 schema

---
+++ src/JsonSchema/Constraints/Drafts/Draft06/FormatConstraint.php
@@ -0,0 +1,171 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JsonSchema\Constraints\Drafts\Draft06;
+
+use JsonSchema\ConstraintError;
+use JsonSchema\Constraints\ConstraintInterface;
+use JsonSchema\Constraints\Factory;
+use JsonSchema\Entity\ErrorBagProxy;
+use JsonSchema\Entity\JsonPointer;
+use JsonSchema\Rfc3339;
+use JsonSchema\Tool\Validator\RelativeReferenceValidator;
+use JsonSchema\Tool\Validator\UriValidator;
+
+class FormatConstraint implements ConstraintInterface
+{
+    use ErrorBagProxy;
+
+    public function __construct(?Factory $factory = null)
+    {
+        $this->initialiseErrorBag($factory ?: new Factory());
+    }
+
+    public function check(&$value, $schema = null, ?JsonPointer $path = null, $i = null): void
+    {
+        if (!property_exists($schema, 'format')) {
+            return;
+        }
+
+        if (!is_string($value)) {
+            return;
+        }
+
+        switch ($schema->format) {
+            case 'date':
+                if (!$this->validateDateTime($value, 'Y-m-d')) {
+                    $this->addError(ConstraintError::FORMAT_DATE(), $path, ['date' => $value, 'format' => $schema->format]);
+                }
+                break;
+            case 'time':
+                if (!$this->validateDateTime($value, 'H:i:s')) {
+                    $this->addError(ConstraintError::FORMAT_TIME(), $path, ['time' => $value, 'format' => $schema->format]);
+                }
+                break;
+            case 'date-time':
+                if (Rfc3339::createFromString($value) === null) {
+                    $this->addError(ConstraintError::FORMAT_DATE_TIME(), $path, ['dateTime' => $value, 'format' => $schema->format]);
+                }
+                break;
+            case 'utc-millisec':
+                if (!$this->validateDateTime($value, 'U')) {
+                    $this->addError(ConstraintError::FORMAT_DATE_UTC(), $path, ['value' => $value, 'format' => $schema->format]);
+                }
+                break;
+            case 'regex':
+                if (!$this->validateRegex($value)) {
+                    $this->addError(ConstraintError::FORMAT_REGEX(), $path, ['value' => $value, 'format' => $schema->format]);
+                }
+                break;
+            case 'ip-address':
+            case 'ipv4':
+                if (filter_var($value, FILTER_VALIDATE_IP, FILTER_NULL_ON_FAILURE | FILTER_FLAG_IPV4) === null) {
+                    $this->addError(ConstraintError::FORMAT_IP(), $path, ['format' => $schema->format]);
+                }
+                break;
+            case 'ipv6':
+                if (filter_var($value, FILTER_VALIDATE_IP, FILTER_NULL_ON_FAILURE | FILTER_FLAG_IPV6) === null) {
+                    $this->addError(ConstraintError::FORMAT_IP(), $path, ['format' => $schema->format]);
+                }
+                break;
+            case 'color':
+                if (!$this->validateColor($value)) {
+                    $this->addError(ConstraintError::FORMAT_COLOR(), $path, ['format' => $schema->format]);
+                }
+                break;
+            case 'style':
+                if (!$this->validateStyle($value)) {
+                    $this->addError(ConstraintError::FORMAT_STYLE(), $path, ['format' => $schema->format]);
+                }
+                break;
+            case 'phone':
+                if (!$this->validatePhone($value)) {
+                    $this->addError(ConstraintError::FORMAT_PHONE(), $path, ['format' => $schema->format]);
+                }
+                break;
+            case 'uri':
+                if (!UriValidator::isValid($value)) {
+                    $this->addError(ConstraintError::FORMAT_URL(), $path, ['format' => $schema->format]);
+                }
+                break;
+
+            case 'uriref':
+            case 'uri-reference':
+                if (!(UriValidator::isValid($value) || RelativeReferenceValidator::isValid($value))) {
+                    $this->addError(ConstraintError::FORMAT_URL(), $path, ['format' => $schema->format]);
+                }
+                break;
+
+            case 'email':
+                if (filter_var($value, FILTER_VALIDATE_EMAIL, FILTER_NULL_ON_FAILURE | FILTER_FLAG_EMAIL_UNICODE) === null) {
+                    $this->addError(ConstraintError::FORMAT_EMAIL(), $path, ['format' => $schema->format]);
+                }
+                break;
+            case 'host-name':
+            case 'hostname':
+                if (!$this->validateHostname($value)) {
+                    $this->addError(ConstraintError::FORMAT_HOSTNAME(), $path, ['format' => $schema->format]);
+                }
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    private function validateDateTime(string $datetime, string $format): bool
+    {
+        $dt = \DateTime::createFromFormat($format, $datetime);
+
+        if (!$dt) {
+            return false;
+        }
+
+        return $datetime === $dt->format($format);
+    }
+
+    private function validateRegex(string $regex): bool
+    {
+        return preg_match(self::jsonPatternToPhpRegex($regex), '') !== false;
+    }
+
+    /**
+     * Transform a JSON pattern into a PCRE regex
+     */
+    private static function jsonPatternToPhpRegex(string $pattern): string
+    {
+        return '~' . str_replace('~', '\\~', $pattern) . '~u';
+    }
+
+    private function validateColor(string $color): bool
+    {
+        if (in_array(strtolower($color), ['aqua', 'black', 'blue', 'fuchsia',
+            'gray', 'green', 'lime', 'maroon', 'navy', 'olive', 'orange', 'purple',
+            'red', 'silver', 'teal', 'white', 'yellow'])) {
+            return true;
+        }
+
+        return preg_match('/^#([a-f0-9]{3}|[a-f0-9]{6})$/i', $color) !== false;
+    }
+
+    private function validateStyle(string $style): bool
+    {
+        $properties     = explode(';', rtrim($style, ';'));
+        $invalidEntries = preg_grep('/^\s*[-a-z]+\s*:\s*.+$/i', $properties, PREG_GREP_INVERT);
+
+        return empty($invalidEntries);
+    }
+
+    private function validatePhone($phone): bool
+    {
+        return preg_match('/^\+?(\(\d{3}\)|\d{3}) \d{3} \d{4}$/', $phone) !== false;
+    }
+
+    private function validateHostname(string $host): bool
+    {
+        $hostnameRegex = '/^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/i';
+
+        return preg_match($hostnameRegex, $host) !== false;
+    }
+}
