eed0a525646e7046f7bf3a7b29e02facd88accb3
Danny van der Sluijs <danny.van.der.sluijs@infi.nl>
2025-08-22 13:36:20 +0200

    style: Correct code style violations

--- tests/JsonSchemaTestSuiteTest.php
+++ tests/JsonSchemaTestSuiteTest.php
@@ -148,12 +148,13 @@ class JsonSchemaTestSuiteTest extends TestCase
             '[draft4/refRemote.json]: base URI change - change folder: string is invalid is expected to be invalid', // Test case was added after v1.2.0, skip test for now.
             '[draft4/refRemote.json]: Location-independent identifier in remote ref: integer is valid is expected to be valid', // Test case was added after v1.2.0, skip test for now.
             '[draft4/refRemote.json]: Location-independent identifier in remote ref: string is invalid is expected to be invalid', // Test case was added after v1.2.0, skip test for now.
-            '[draft6/ref.json]: Location-independent identifier: mismatch is expected to be invalid', // Test case was skipped from draft4, skip for draft6 as well.
-            '[draft6/ref.json]: Location-independent identifier with base URI change in subschema: mismatch is expected to be invalid', // Test case was skipped from draft4, skip for draft6 as well.
-            '[draft6/ref.json]: refs with quote: object with strings is invalid is expected to be invalid', // Test case was skipped from draft4, skip for draft6 as well.
-            '[draft6/ref.json]: empty tokens in $ref json-pointer: non-number is invalid is expected to be invalid', // Test case was skipped from draft4, skip for draft6 as well.
-            '[draft6/refRemote.json]: base URI change - change folder: string is invalid is expected to be invalid', // Test case was skipped from draft4, skip for draft6 as well.
-            '[draft6/refRemote.json]: Location-independent identifier in remote ref: string is invalid is expected to be invalid', // Test case was skipped from draft4, skip for draft6 as well.
+            '[draft6/ref.json]: Location-independent identifier with base URI change in subschema: mismatch is expected to be invalid', // Test case was added after v1.2.0, skip test for now.
+            '[draft6/ref.json]: Location-independent identifier: mismatch is expected to be invalid', // Same test case is skipped for draft4, skip for now as well.
+            '[draft6/ref.json]: refs with quote: object with strings is invalid is expected to be invalid', // Same test case is skipped for draft4, skip for now as well.
+            '[draft6/ref.json]: empty tokens in $ref json-pointer: non-number is invalid is expected to be invalid', // Same test case is skipped for draft4, skip for now as well.
+            '[draft6/refRemote.json]: base URI change - change folder: string is invalid is expected to be invalid', // Same test case is skipped for draft4, skip for now as well.
+            '[draft6/refRemote.json]: Location-independent identifier in remote ref: string is invalid is expected to be invalid', // Same test case is skipped for draft4, skip for now as well.
+
         ];
 
         if ($this->is32Bit()) {
