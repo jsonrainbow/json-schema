jsonrainbow/json-schema

commit b1562e41c948dc1a5a4ec6b73844f7af2e85a70c

Fix PHP 8.5 $http_response_header deprecation (#841)
Replaces #840 (due to blocked write access on fork)

---------

Co-authored-by: Andy Postnikov <apostnikov@gmail.com>

src/JsonSchema/Uri/Retrievers/FileGetContents.php
@@ -51,6 +51,11 @@ class FileGetContents extends AbstractRetriever
         }
 
         $this->messageBody = $response;
+        if (function_exists('http_get_last_response_headers')) {
+            // Use http_get_last_response_headers() for BC compatibility with PHP 8.5+
+            // where $http_response_header is deprecated.
+            $http_response_header = http_get_last_response_headers();
+        }
         if (!empty($http_response_header)) {
             // $http_response_header cannot be tested, because it's defined in the method's local scope
             // See http://php.net/manual/en/reserved.variables.httpresponseheader.php for more info.

commit def86e58343e1087d1edab83a0b575f38cc34659

Fix issue when http headers are already present (#843)
This fixes a regression introduced in
https://github.com/jsonrainbow/json-schema/pull/841

If a process/request runs:

- file_get_contents on an HTTP URL.
- then a json-schema validation using a file://... URL as schema (not
all schema are remotely loaded)
- and `http_get_last_response_headers()` is available

Then you end up with:

- file_get_contents on the URL fills up the headers of that request
- json-schema validation loads the schema file but using file:// there
are no headers, and it seems like file_get_contents does not clear
previous headers itself
- http_get_last_response_headers() then returns the headers of the
previous request
- and then `$this->fetchContentType()` will fail unless the response
headers were of the correct `application/schema+json` mime type (which
is very unlikely).

So this PR clears the headers before we fetch the schema, to make sure
we have a clean slate. I'll probably report this to PHP as well because
IMO it is a bit surprising.

---------

Co-authored-by: Danny van der Sluijs <danny.van.der.sluijs@infi.nl>

src/JsonSchema/Uri/Retrievers/FileGetContents.php
@@ -29,6 +29,10 @@ class FileGetContents extends AbstractRetriever
      */
     public function retrieve($uri)
     {
+        if (function_exists('http_clear_last_response_headers')) {
+            http_clear_last_response_headers();
+        }
+
         $errorMessage = null;
         set_error_handler(function ($errno, $errstr) use (&$errorMessage) {
             $errorMessage = $errstr;