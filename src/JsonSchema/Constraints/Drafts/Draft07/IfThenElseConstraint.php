b0030d10cfdb50ff5c98affc6fd8fcf8e528f77f
Danny van der Sluijs <danny.van.der.sluijs@infi.nl>
2025-10-10 15:38:18 +0200

    style: Correct code style violations

--- src/JsonSchema/Constraints/Drafts/Draft07/IfThenElseConstraint.php
+++ src/JsonSchema/Constraints/Drafts/Draft07/IfThenElseConstraint.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace JsonSchema\Constraints\Drafts\Draft07;
 
-use JsonSchema\ConstraintError;
 use JsonSchema\Constraints\ConstraintInterface;
 use JsonSchema\Entity\ErrorBagProxy;
 use JsonSchema\Entity\JsonPointer;
@@ -50,6 +49,7 @@ class IfThenElseConstraint implements ConstraintInterface
             }
 
             $this->addErrors($schemaConstraint->getErrors());
+
             return;
         }
 
