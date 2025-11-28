7024d29d2b55603c0a2dcdaaa062d7f48b971741
Dave Long <dave@longwaveconsulting.com>
2025-11-28 14:57:33 +0000

    Update tests/Tool/Validator/UriValidatorTest.php
    Co-authored-by: Danny van der Sluijs <danny.vandersluijs@icloud.com>

--- tests/Tool/Validator/UriValidatorTest.php
+++ tests/Tool/Validator/UriValidatorTest.php
@@ -32,7 +32,8 @@ class UriValidatorTest extends TestCase
         yield 'Data URI' => ['uri' => 'data:text/plain;charset=utf-8,Hello%20World!'];
         yield 'ISBN URN URI' => ['uri' => 'urn:isbn:0451450523'];
         yield 'OASIS URN URI' => ['uri' => 'urn:oasis:names:specification:docbook:dtd:xml:4.1.2'];
-        yield 'Custom URI' => ['uri' => 'custom://reg_name/path/file.json'];
+        yield 'Custom URI with underscore' => ['uri' => 'custom://reg_name/path/file.json'];
+        yield 'Custom URI with tilde' => ['uri' => 'custom://reg~name/path/file.json'];
     }
 
     public function invalidUriDataProvider(): \Generator
