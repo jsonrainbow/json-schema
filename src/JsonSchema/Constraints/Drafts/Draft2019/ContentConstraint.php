a6f9a162b95e317affef4f114569cb74efa380cb
Danny van der Sluijs <danny.van.der.sluijs@infi.nl>
2026-02-18 21:33:15 +0100

    fix: Skip validation of content due to security concerns

--- src/JsonSchema/Constraints/Drafts/Draft2019/ContentConstraint.php
+++ src/JsonSchema/Constraints/Drafts/Draft2019/ContentConstraint.php
@@ -21,35 +21,6 @@ class ContentConstraint implements ConstraintInterface
 
     public function check(&$value, $schema = null, ?JsonPointer $path = null, $i = null): void
     {
-        if (!property_exists($schema, 'contentMediaType') && !property_exists($schema, 'contentEncoding')) {
-            return;
-        }
-        if (!is_string($value)) {
-            return;
-        }
-
-        $decodedValue = $value;
-
-        if (property_exists($schema, 'contentEncoding')) {
-            if ($schema->contentEncoding === 'base64') {
-                if (!preg_match('/^[A-Za-z0-9+\/=]+$/', $decodedValue)) {
-                    $this->addError(ConstraintError::CONTENT_ENCODING(), $path, ['contentEncoding' => $schema->contentEncoding]);
-
-                    return;
-                }
-                $decodedValue = base64_decode($decodedValue);
-            }
-        }
-
-        if (property_exists($schema, 'contentMediaType')) {
-            if ($schema->contentMediaType === 'application/json') {
-                json_decode($decodedValue, false);
-                if (json_last_error() === JSON_ERROR_NONE) {
-                    return;
-                }
-            }
-
-            $this->addError(ConstraintError::CONTENT_MEDIA_TYPE(), $path, ['contentMediaType' => $schema->contentMediaType]);
-        }
+        // See https://json-schema.org/draft/2019-09/json-schema-validation#rfc.section.8
     }
 }
